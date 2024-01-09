6012c747c910d6514f9f582ab5395ae4997a2c1f
Your Name <beloshapkobm@yandex.ru>
2024-01-09 00:45:43 -0300

    Day 1

---
+++ 100 Day of code challenge/Day 1/main.py
@@ -0,0 +1,19 @@
+#Python practice
+#Day 1 / 100
+
+#fun
+deco = '*' * 10
+
+#Welcome text as a string
+welcome_text = " Welcome to the Band Name Generator. "
+print(deco + welcome_text + deco)
+
+#First variable
+name_of_the_city = input("What's name of the city you grew up in? :  ")
+
+#Second variable
+name_of_your_pet = input("What's your pet's name? :  ")
+
+#Generate
+print(name_of_the_city + ' ' + name_of_your_pet)
+
