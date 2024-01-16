a9c352b4afa500617fb7649850b18e482d396a24
Your Name <beloshapkobm@yandex.ru>
2024-01-16 20:45:29 -0300

    Day 6/7

---
+++ 100 Day of code challenge/Day 6/Practice1.py
@@ -0,0 +1,15 @@
+
+
+print("Hello")
+
+num_char = len("hello")
+print(num_char)
+
+def my_function():
+    #Do this
+    #Then do this
+    #Finally do this
+    print("Hello")
+    print("Bye")
+
+my_function()
