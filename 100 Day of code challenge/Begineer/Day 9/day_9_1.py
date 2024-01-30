cc408f464b82ee57af69273991cb14cfdc7574b0
Your Name <beloshapkobm@yandex.ru>
2024-01-30 18:36:57 -0300

    day 15/Module trainig (Flask)

---
+++ 100 Day of code challenge/Begineer/Day 9/day_9_1.py
@@ -0,0 +1,55 @@
+# programming_dictionary = {
+#     'Bug': "An error in a program that prevents the program from running as expected.",
+#     'Function': "A piece of code that you can easily call and over again.",
+# }
+#
+# print(programming_dictionary['Bug'])
+#
+# programming_dictionary['Loop'] = "The action of doing something over and over again."
+#
+# print(programming_dictionary['Loop'])
+#
+# empty_dictionary = {}
+#
+# # programming_dictionary = {}
+# # print(programming_dictionary)
+#
+# # programming_dictionary['Bug'] = "A moth in your computer"
+# # print(programming_dictionary)
+#
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+#
+#
+
+capitals = {
+    'France': 'Paris',
+    'Germany': 'Berlin',
+}
+
+travel_log = {
+    'Frace': {
+        'cities_visited': ['Paris', 'Lille', 'Dijon'],
+        'total_visits': 12
+    },
+    'Germany': {
+        'cities_visited': ['Berlin', 'Hamburg', 'Stuttgart'],
+        'total_visits': 5
+    },
+}
+
+travel_log = [
+    {
+    'country': 'Frace',
+    'cities_visited': ['Paris', 'Lille', 'Dijon'],
+    'total_visits': 12
+    },
+    {
+    'country': 'Germany',
+    'cities_visited': ['Berlin', 'Hamburg', 'Stuttgart'],
+    'total_visits': 5
+    },
+]
+
+
